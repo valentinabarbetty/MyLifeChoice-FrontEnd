075c1bfd2d7ef03343488d767803f0b7e51ab2f1
valentinabarbetty <valentinabarbetty2@gmail.com>
2025-11-09 21:48:18 -0500

    Google Sign up from landing done

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,12 @@ import IntroFlow from './pages/IntroFlow/IntroFlow.jsx'
 import { GoogleOAuthProvider } from "@react-oauth/google"
 import './index.css'
 
-// 🚀 Agrega tu Client ID de Google aquí:
-const GOOGLE_CLIENT_ID = "183624025015-56bgdj86f3ejt6uqvus8f1e1ajqq3ubt.apps.googleusercontent.com"
+
+//const GOOGLE_CLIENT_ID = "183624025015-dpp2h6idnrj3o9h68fqiq2nbea2be3dq.apps.googleusercontent.com"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
@@ -20,6 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/intro" element={<IntroFlow />} />
         </Routes>
       </BrowserRouter>
-    </GoogleOAuthProvider>
+
   </React.StrictMode>
 )
